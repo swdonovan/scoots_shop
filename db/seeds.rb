0d6e76b54d65e7211d5ffd9e8a709415cf4667e7
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-27 13:37:08 -0600

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::Commerce.product_name
+  price = Faker::Commerce.price
+  description = Faker::HowIMetYourMother.quote
+  image = Faker::LoremPixel.image
+  Item.create(title: title, price: price, description: description, image: image)
+end
+
+item_ids = Item.pluck(:id)
+
+5.times do
+  name = Faker::Job.field
+  category = Category.create(name: name)
+  2.times do
+    category.items << Item.find(item_ids.sample)
+  end
+end
