cc695f3b02ef506e8a0049c305ae5c7226252567
Wil Donovan <wil.donovan@gmail.com>
2017-07-28 17:45:59 -0600

    here is the updated user controller with very raw factoring....i think a before_action and use either current user or a similar method would be wise....will refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if session[:user_id] == @user.id
+      render 'show'
+    else
+      session[:user_id] = nil
+      redirect_to login_path
+    end
   end
 end
