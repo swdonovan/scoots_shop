1517c921a7faa064bc194788e8ee6abd49e60e8b
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-28 17:21:39 -0600

    add show page for retired item

---
+++ spec/features/items/user_can_see_a_retired_item_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature 'User visits a page for a retired item' do
+  scenario 'and sees the item but cannot add it to the cart' do
+    user = create(:user)
+    item = create(:item, role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).
+    and_return(user)
+    visit item_path(item)
+
+    expect(current_path).to eq "/items/#{item.id}"
+    expect(page).to have_content item.title
+    expect(page).to have_content item.description
+    expect(page).to have_css "img[src='#{item.image}']"
+    expect(page).to have_content item.price
+    expect(page).to have_content item.role
+    expect(page).to_not have_button "Add to Cart"
+    expect(page).to have_button 'Item Retired', disabled: true
+  end
+end
