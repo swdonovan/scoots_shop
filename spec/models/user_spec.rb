95adb6ee8fadfb2588d9849c181e8284795eb2e3
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-29 07:24:45 -0600

    remove address validation, add unique attribute to username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it {is_expected.to validate_presence_of(:username)}
     it {is_expected.to validate_presence_of(:password)}
-    it {is_expected.to validate_presence_of(:address)}
     it {is_expected.to validate_presence_of(:email)}
+
+    it {is_expected.to validate_uniqueness_of(:username)}
   end
 end
