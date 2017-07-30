84ca23fd05ebaa778dcafff04abedb2223654e42
Daniel Phillips <danieledward.phillips@gmail.com>
2017-07-30 16:02:22 -0600

    adds test for auth and auth for user trying to see others private info and for trying to access admin stuff

---
+++ spec/features/users/unauth_user_cant_see_private_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Unauthenticated user" do
+  scenario "user can't see others' private data" do
+
+   visit orders_path
+
+   expect(current_path).to eq(login_path)
+   expect(page).to have_content("You must login to see your orders")
+  end
+  skip "user should be redirected to login if tries to checkout" do
+    #this is dependent on user story 15 (checkout)
+
+    #code:
+    #visit checkout or whatever
+    #redireted to login page
+
+
+  end
+  scenario "unauthorized/unauthenticated user tries to make self admin" do
+
+    visit admin_user_path(1)
+
+
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    save_and_open_page
+  end
+end
