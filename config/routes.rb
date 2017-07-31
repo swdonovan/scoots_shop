6d17c7dfe0c67eff5e1abd98a12bcac2833858af
Wil Donovan <wil.donovan@gmail.com>
2017-07-30 18:01:40 -0600

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
   root to: 'items#index'
+
   get '/cart', to: 'carts#index'
   delete '/carts', to: 'carts#destroy'
   patch '/carts', to: 'carts#update'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/login', to: 'sessions#destroy'
+
   resources :items, only: [:index, :show]
+
   get '/dashboard', to: 'users#show'
+
   resources :carts, only: [:create]
 
   resources :orders, only: :index
 
   resources :users, only: [:new, :create]
+
+  get '/checkout', to: 'order#new'
+  post '/checkout', to: 'order#create'
 
   #reassure the dynamic get command is properly routed towards the end
   get "/:category_name", to: 'categories#show'
