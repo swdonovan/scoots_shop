7b2c70c71e019d76b42b24d220359bfed8ddc010
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-31 12:43:20 -0600

    add flash messages

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,6 +5,7 @@ class CartsController < ApplicationController
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
     session[:cart] = @cart.contents
+    flash[:success] = "Added #{item.title} to your cart"
 
     redirect_to request.referrer
   end
