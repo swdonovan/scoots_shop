d07ec34db6ba705e2bb4ed6efdcf01b8073163ca
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-31 13:14:20 -0600

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Item.destroy_all
+Category.destroy_all
+User.destroy_all
+Orders.destroy_all
+
+admin = User.create(username: 'mikelsage', password: 'password', email: 'michael@scoots.com', role: 1)
+user = User.create(username: 'user', password: 'user', email: 'user@scoots.com')
+
 item1 = Item.create(title: 'Scooter', description: 'Its a scooter', price: 39.99)
 item2 = Item.create(title: 'Faster Scooter', description: 'Its a faster scooter', price: 39.99)
 item3 = Item.create(title: 'Slower Scooter', description: 'Its a slower scooter', price: 39.99)
